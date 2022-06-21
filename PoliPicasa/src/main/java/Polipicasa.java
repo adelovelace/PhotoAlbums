fed7ae999857940c61f11de5f9f73963f07823bf
adelovelace <anmero@fiec.espol.edu.ec>
2022-06-21 12:14:25 -0500

    add main class Polipicasa

---
+++ PoliPicasa/src/main/java/Polipicasa.java
@@ -0,0 +1,20 @@
+/* Polipicasa
+ *
+ * @authors
+ * Donoso Bravo, Luis
+ * Mero Plaza, Andrea
+ * Peñafiel Labanda, Alex
+ *
+ */
+
+
+public class Polipicasa {
+
+
+    public static void main(String[] args) {
+
+    }
+
+
+
+}
