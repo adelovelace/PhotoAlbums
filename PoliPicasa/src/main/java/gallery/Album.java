722340c912fa0132453dbd260ec4f3e5d7b1d8a9
adelovelace <anmero@fiec.espol.edu.ec>
2022-06-21 12:53:38 -0500

    add method showPhotos

--- PoliPicasa/src/main/java/gallery/Album.java
+++ PoliPicasa/src/main/java/gallery/Album.java
@@ -1,15 +1,46 @@
 package gallery;
 
+import user.Person;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class Album <T> {
 
+
+
     private String albumName;
     private String albumDescription;
+
+    private ArrayList<Photo> photosOnAlbum;
 
     public Album(String albumName,String albumDescription){
         this.albumName = albumName;
         this.albumDescription = albumDescription;
+    }
+
+
+    public boolean showPhotos(ArrayList photosOnAlbum){
+
+        if(photosOnAlbum ==null){
+            return false;
+        }
+
+        this.photosOnAlbum = photosOnAlbum;
+
+        for (Photo photo: this.photosOnAlbum) {
+            //console testing
+            System.out.println("Photo Info on " + this.albumName + " Album");
+            System.out.println("Photo description:" + photo.getDescriptionPhoto());
+            System.out.println("Place:" + photo.getPlacePhoto());
+            System.out.println("Date:" + photo.getDatePhoto());
+
+            for (Person person: photo.getPersonsOnAlbum()) {
+                System.out.println("Name of the Person:" + person.getPersonName());
+            }
+        }
+
+        return true;
     }
 
     public String getAlbumName() {
