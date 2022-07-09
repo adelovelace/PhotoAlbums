36b75863b81f2745f5992eba9c0c415998ed428e
adelovelace <anmero@fiec.espol.edu.ec>
2022-07-09 16:10:36 -0500

    changes of the cdl implementation

--- PoliPicasa/src/main/java/util/CircularDoublyLinkedList.java
+++ PoliPicasa/src/main/java/util/CircularDoublyLinkedList.java
@@ -18,7 +18,6 @@ public class CircularDoublyLinkedList<E> implements List<E> {
     private Node <E> head;
     private Node <E> tail;
 
-    ;
     private int listSize = 0;
 
     public CircularDoublyLinkedList() {
@@ -37,82 +36,222 @@ public class CircularDoublyLinkedList<E> implements List<E> {
 
         if(this.listSize == 0){
             this.head = newNode;
-            this.head.prev = this.head;
-            this.head.next = this.head;
-            listSize ++;
+            this.tail = newNode;
+            this.listSize ++;
             return true;
         }
 
         if(this.head == null){
             this.head = newNode;
-            this.head.prev = this.head;
-            this.head.next = this.head;
-            listSize ++;
+            this.tail = newNode;
+            this.listSize ++;
             return true;
         }
 
-        newNode.prev = this.head;
-        this.head.next = newNode;
-        listSize++;
+        newNode.next = this.head;
+        newNode.prev = this.tail;
+        this.head.prev = newNode;
+        this.tail.next = newNode;
+        this.tail= newNode;
+
+        this.listSize++;
 
         return true;
     }
 
     @Override
     public boolean addLast(E e) {
-        return false;
+
+        Node <E> newNode = new Node<E>(e);
+
+        if(e ==null){
+            return false;
+        }
+
+        if(head==null){
+            head= newNode;
+            tail=newNode;
+            this.listSize = 1;
+            return true;
+        }
+
+        newNode.prev = tail;
+        newNode.next = head;
+        tail.next = newNode;
+        head.prev = newNode;
+        tail = newNode;
+        this.listSize ++;
+
+        return true;
+
     }
 
     @Override
     public E getFirst() {
-        return null;
+
+
+        if(this.listSize == 0){
+            return null;
+        }
+
+        if(this.head == null){
+            return null;
+        }
+
+        return this.head.data;
     }
 
     @Override
     public E getLast() {
-        return null;
+
+        if(this.listSize == 0){
+            return null;
+        }
+
+        if(this.tail == null){
+            return null;
+        }
+
+        return this.tail.data;
     }
 
     @Override
     public int indexOf(E e) {
-        return 0;
+
+        int index = 0;
+
+        if(e == null){
+            return index;
+        }
+
+        for(int i = 0; i < this.listSize; i++){
+            if(this.head.data == e){
+                index = i;
+            }
+        }
+
+        return index;
     }
 
     @Override
     public int size() {
 
-        return listSize;
+        return this.listSize;
     }
 
     @Override
     public boolean removeLast() {
-        return false;
+
+
+
+        if(this.listSize == 0){
+            return false;
+        }
+
+        if(this.tail == null){
+            return false;
+        }
+
+        this.head.prev = this.tail.prev;
+        this.tail.prev = this.tail.next;
+        this.tail.prev = this.tail;
+        this.listSize --;
+
+        return true;
     }
 
     @Override
 
     public boolean removeFirst() {
-        return false;
+
+        if(this.listSize == 0){
+            return false;
+        }
+
+        if(this.head == null){
+            return false;
+        }
+
+        this.tail.next = this.head.next;
+        this.head.next = this.head.prev;
+        this.head.next = this.head;
+        this.listSize --;
+
+        return true;
 
     }
 
     @Override
     public boolean insert(int index, E e) {
 
+        Node <E> newNode = new Node<>(e);
 
+        if(e == null){
+            return false;
+        }
+
+        if(index < 0 || index >= this.listSize){
+            throw new IndexOutOfBoundsException("Indice no está dentro del rango");
+        }
+
+        for (int i = 0; i < this.listSize; i++){
+
+            if(index ==0){
+                return addFirst(newNode.data);
+            }
+
+            if(index == this.listSize-1){
+                return addLast(newNode.data);
+            }
+
+            if(index == i){
+                Node <E> actualNode = new Node<>(get(i));
+                newNode.prev = actualNode.prev;
+                actualNode.prev = newNode;
+                newNode.next = actualNode;
+                this.listSize++;
+                return true;
+            }
+        }
 
         return false;
     }
 
     @Override
     public boolean set(int index, E e) {
+
+        if(e == null){
+            return false;
+        }
+
+        if(index < 0 || index >= this.listSize){
+            throw new IndexOutOfBoundsException("Indice no está dentro del rango");
+        }
+
+        for (int i = 0; i < this.listSize; i++){
+
+            if(index ==0){
+                return addFirst(e);
+            }
+
+            if(index == this.listSize-1){
+                return addLast(e);
+            }
+
+            if(index == i){
+                Node <E> actualNode = new Node<>(get(index));
+                actualNode.data = e;
+                return true;
+            }
+        }
+
         return false;
     }
 
     @Override
     public boolean isEmpty() {
 
-        if(this.head == null){
+        if(this.listSize == 0){
             return true;
         }
 
@@ -121,16 +260,79 @@ public class CircularDoublyLinkedList<E> implements List<E> {
 
     @Override
     public E get(int index) {
+
+        if(index < 0 || index >= this.listSize){
+            throw new IndexOutOfBoundsException("Indice no está dentro del rango");
+        }
+
+        for (int i = 0; i < this.listSize; i++){
+
+            if(index ==0){
+                return getFirst();
+            }
+
+            if(index == this.listSize-1){
+                return getLast();
+            }
+
+            if(index == i){
+                Node <E> actualNode = new Node<>(get(index));
+                return actualNode.data;
+            }
+        }
+
         return null;
     }
 
     @Override
     public boolean contains(E e) {
+
+        Node actualNode = this.head;
+
+        if(e == null){
+            return false;
+        }
+
+        do{
+
+            if(actualNode.data == e){
+                return true;
+            }
+
+            actualNode = actualNode.next;
+
+        }while(actualNode != this.head);
+
         return false;
     }
 
     @Override
     public boolean remove(int index) {
+
+        if(index < 0 || index >= this.listSize){
+            throw new IndexOutOfBoundsException("Indice no está dentro del rango");
+        }
+
+        for (int i = 0; i < this.listSize; i++){
+
+            if(index ==0){
+                return removeFirst();
+            }
+
+            if(index == this.listSize-1){
+                return removeLast();
+            }
+
+            if(index == i){
+                Node <E> actualNode = new Node<>(get(index));
+                Node <E> temNode = actualNode.next;
+                actualNode.prev = actualNode.next;
+                actualNode.next = temNode.prev;
+                this.listSize--;
+                return true;
+            }
+        }
+
         return false;
     }
 }
